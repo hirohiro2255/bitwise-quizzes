d3ac2d213e8280679b6a69a844414ea34adfa989
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-04-02 20:50:14 +0900

    Create some styling for structual layout on desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import { genQuizzes } from '../utils/utils';
   let quizzes: Quiz[] = [];
   let solutionID = 0;
+  let ownSolution = '';
 
   onMount(() => {
     quizzes = [...genQuizzes()];
@@ -22,24 +23,77 @@
 </header>
 
 <main class="container">
-  <section>
-    <div>
-      <p />
-    </div>
-    <div>
+  <section class="quiz-container">
+    <nav>
       <ul>
+        <li>{solutionID + 1} / {quizzes.length}</li>
+        <li><i class="fa-solid fa-calculator" /></li>
+      </ul>
+    </nav>
+    <div class="quiz-interaction-group">
+      {#if quizzes.length !== 0}
         {#each quizzes as quiz, i}
           {#if i === solutionID}
-            <li>{quiz.left} {quiz.op} {quiz.right} = ?</li>
+            <h1 class="quiz-title">{quiz.left} {quiz.op} {quiz.right} = ?</h1>
           {/if}
         {/each}
-      </ul>
+      {:else}
+        <h1 class="quiz-title">Loading...</h1>
+      {/if}
+      <div class="input-group">
+        <input type="text" on:input={(e) => console.log(e)} />
+        <button class="button">Next</button>
+      </div>
     </div>
+  </section>
+  <section class="result-container">
+    <h2>Result:</h2>
   </section>
 </main>
 
 <style>
   .nav-list {
     display: flex;
+  }
+
+  .container {
+    display: flex;
+    height: 83%;
+    margin-top: 1.2rem;
+  }
+
+  .quiz-container {
+    flex: 5;
+    border: 1px solid red;
+    /* display: flex;
+    flex-direction: column; */
+    /* justify-content: space-between; */
+  }
+
+  .quiz-container nav {
+    height: 10%;
+  }
+
+  .quiz-container nav ul {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  .quiz-interaction-group {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 8rem;
+  }
+
+  .quiz-title {
+    font-size: 3.2rem;
+    color: #434146;
+  }
+
+  .result-container {
+    flex: 3;
+    border: 1px solid blue;
   }
 </style>
